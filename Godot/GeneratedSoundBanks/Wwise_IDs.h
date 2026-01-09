6cc2fe6c8188d620f81ff93f7a659b3f3d989349
Permadeaf <101829691+Permadeaf@users.noreply.github.com>
2026-01-09 09:51:05 -0600

    Updated Soundbank folder
    Regenerated Soundbanks

--- Godot/GeneratedSoundBanks/Wwise_IDs.h
+++ Godot/GeneratedSoundBanks/Wwise_IDs.h
@@ -232,6 +232,7 @@ namespace AK
         static const AkUniqueID DIALOGUE = 3930136735U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
         static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID REVERB = 348963605U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID SOUND_MASTER = 2645002259U;
         static const AkUniqueID UNDERWATER = 2213237662U;
